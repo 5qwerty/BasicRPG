5833be0c8491a88c6806756d5fd6e1fa3a7b3689
Sean Lazaro <sean.lazaro@edu.uwaterloo.ca>
2019-01-05 19:12:15 -0500

    Moving between areas

---
+++ Assets/Scripts/GoToOtherMap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToOtherMap : MonoBehaviour
+{
+    public string destinationScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(destinationScene);
+        }
+    }
+}
